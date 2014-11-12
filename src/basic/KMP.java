789bf40a6863cffe63505e8ae699778d4924954c
Yi <yil712@lehigh.edu>
2014-11-12 00:53:34 -0500

    Added some classes for strings.

---
+++ src/basic/KMP.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class KMP {
+	/**
+	 * O(n)
+	 */
+	public static int KMPSearch(String s, String p, int[] next) {
+		return -1;
+	}
+	
+	/**
+	 * Pre-processing, O(m)
+	 */
+	public static int[] getNextArray(String p) { 
+		return null;
+	}
+}
